ea3f7f721dc51d420b911750d5b3012ec7552540
Anthony Williams <anthony@ninecraft.com>
2009-03-02 23:57:46 +0000

    No need for any load blocks.

--- lib/showcase-common.rb
+++ lib/showcase-common.rb
@@ -10,13 +10,5 @@ if defined?(Merb::Plugins)
     :nav_formatter => Showcase::Common::Nav::BasicFormatter
   }
 
-  Merb::BootLoader.before_app_loads do
-    # require code that must be loaded before the application
-  end
-
-  Merb::BootLoader.after_app_loads do
-    # code that can be required after the application loads
-  end
-
   # Merb::Plugins.add_rakefiles "showcase-common/merbtasks"
 end
