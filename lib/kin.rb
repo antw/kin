aa361ffd10ecdd48816d7a328fa06f46f54729fe
Anthony Williams <anthony@ninecraft.com>
2009-08-22 19:16:51 +0100

    Autoload navigation.rb from config/ when possible.

--- lib/kin.rb
+++ lib/kin.rb
@@ -13,4 +13,11 @@ if defined?(Merb::Plugins)
   # Default nav formatter. Can be overridden in an after_app_loads block, or
   # on a case-by-case basis in +display_navigation+.
   Merb::Plugins.config[:kin] = { :nav_formatter => Kin::Nav::BasicFormatter }
+
+  # After the application loads, see if there is a config/navigation.rb file
+  # to initialize the nav instances.
+  Merb::BootLoader.after_app_loads do
+    nav_rb = File.join(Merb.dir_for(:config), 'navigation.rb')
+    load(nav_rb) if File.exists?(nav_rb)
+  end
 end
